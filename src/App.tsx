1d40d2223bbb797aa9c20f69d3993c02dc940fce
maverick38280 <52314251+maverick38280@users.noreply.github.com>
2019-06-28 15:22:28 +0200

    Add Layout and SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 
-import ROUTE from './constants/ROUTES';
+import ROUTES from './constants/ROUTES';
 
 import { PageNotFound } from './containers/PageNotFound';
 import { LayoutMain } from './containers/LayoutMain';
@@ -9,24 +9,18 @@ import { Home } from './containers/Home';
 import { RecipeList } from './containers/RecipeList';
 import { Recipe } from './containers/Recipe';
 
-import './App.css';
+import './App.scss';
 
 const App: React.FC = () => {
   return (
     <>
       <LayoutMain>
       </LayoutMain>
-      <ul>
-        <li><Link to={ROUTE.HOME}>Home</Link></li>
-        <li><Link to={ROUTE.RECIPE_LIST}>RecipeList</Link></li>
-        <li><Link to={ROUTE.RECIPE + "/test"}>Recipe</Link></li>
-        <li><Link to={ROUTE.PAGE_NOT_FOUND}>404</Link></li>
-      </ul>
       <div>
         <Switch>
-          <Route path={ROUTE.HOME} component={Home} exact />
-          <Route path={ROUTE.RECIPE_LIST} component={RecipeList} exact />
-          <Route path={ROUTE.RECIPE + ROUTE.PARAM_ID} component={Recipe} exact />
+          <Route path={ROUTES.HOME} component={Home} exact />
+          <Route path={ROUTES.RECIPE_LIST} component={RecipeList} exact />
+          <Route path={ROUTES.RECIPE + ROUTES.PARAM_ID} component={Recipe} exact />
           <Route component={PageNotFound} />
         </Switch>
       </div>
